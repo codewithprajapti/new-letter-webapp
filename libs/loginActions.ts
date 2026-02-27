57d381c30bdcac493b139da47b2cf9bfddd04768
Parth Prajapati <parthprajapti2203@gmail.com>
2026-02-27 16:58:01 +0530

    Completed Custom Login functionality using auth.js

--- libs/loginActions.ts
+++ libs/loginActions.ts
@@ -3,11 +3,6 @@
 import { signIn } from "@/auth";
 
 export async function loginAction(formData: FormData) {
-  const email = formData.get("email");
-  const password = formData.get("password");
-
-  console.log(email, password);
-
   await signIn("credentials", {
     email: formData.get("email"),
     password: formData.get("password"),
