b6d490fa43ff5977de212d3593c4eb22c269e2c8
Parth Prajapati <parthprajapti2203@gmail.com>
2026-02-27 15:09:53 +0530

    completed custom credentials functionality

--- libs/loginActions.ts
+++ libs/loginActions.ts
@@ -1,10 +1,16 @@
 "use server";
 
+import { signIn } from "@/auth";
+
 export async function loginAction(formData: FormData) {
   const email = formData.get("email");
   const password = formData.get("password");
 
   console.log(email, password);
 
-  // your authentication logic here
+  await signIn("credentials", {
+    email: formData.get("email"),
+    password: formData.get("password"),
+    redirectTo: "/",
+  });
 }
